8b5d3f5d5a2d6f9dfaed9d7484bde0fd6c8d19a3
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-07 23:31:59 -0300

    Fixing test

--- spec/test_runner_spec.rb
+++ spec/test_runner_spec.rb
@@ -9,7 +9,7 @@ describe TestRunner do
       let(:result) { runner.run_compilation!(file) }
 
       it { expect(result[1]).to eq :failed }
-      it { expect(result[0]).to include 'AssertionException: Expected [4] but found [1]' }
+      it { expect(result[0]).to include 'Expected <[0mtrue[0m> but was <[0mfalse[0m>' }
     end
 
     context 'on passed submission' do
