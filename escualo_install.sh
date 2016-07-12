24a40380d73aeae7d5eac286d8f24f5b26d63a25
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-07-11 21:30:52 -0300

    Removing escualo config

--- escualo_install.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-REV=$1
-
-echo "[Escualo::CSpecServer] Fetching GIT revision"
-echo -n $REV > version
-
-echo "[Escualo::CSpecServer] Pulling docker image"
-docker pull mumuki/mumuki-cspec-worker
