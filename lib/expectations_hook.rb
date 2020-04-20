4316118e1c7a362d8668809ebf666b5921626765
Federico Scarpa <fedescarpa@gmail.com>
2020-04-20 18:38:53 -0300

    Add more specs

--- lib/expectations_hook.rb
+++ lib/expectations_hook.rb
@@ -1,7 +1,15 @@
 class CExpectationsHook < Mumukit::Templates::MulangExpectationsHook
-  include_smells false
+  include_smells true
 
   def language
     'C'
+  end
+
+  def default_smell_exceptions
+    LOGIC_SMELLS + FUNCTIONAL_SMELLS + OBJECT_ORIENTED_SMELLS + ['HasWrongCaseIdentifiers']
+  end
+
+  def parse_response(response)
+    super.uniq
   end
 end
--- spec/expectations_spec.rb
+++ spec/expectations_spec.rb
@@ -12,12 +12,19 @@ describe CExpectationsHook do
   let(:runner) { CExpectationsHook.new }
   let(:result) { compile_and_run(req(expectations, code)) }
 
+  describe 'HasTooShortIdentifiers' do
+    let(:code) { "int f(int x, int y) { return x + y; }" }
+    let(:expectations) { [] }
+
+    it { expect(result).to eq [{expectation: {binding: 'f', inspection: 'HasTooShortIdentifiers'}, result: false}] }
+  end
+
   describe 'DeclaresFunction' do
     let(:code) { "int foo(int x, int y) { return x + y; }\nint bar = 4;" }
     let(:expectations) { [
         {binding: '*', inspection: 'DeclaresFunction:foo'},
         {binding: '*', inspection: 'DeclaresFunction:bar'},
-        {binding: '*', inspection: 'DeclaresFunction:baz'}] }
+        {binding: '*', inspection: 'DeclaresFunction:zaz'}] }
 
     it { expect(result).to eq [
                                   {expectation: expectations[0], result: true},
@@ -30,7 +37,7 @@ describe CExpectationsHook do
     let(:expectations) { [
         {binding: '*', inspection: 'DeclaresVariable:foo'},
         {binding: '*', inspection: 'DeclaresVariable:bar'},
-        {binding: '*', inspection: 'DeclaresVariable:baz'}] }
+        {binding: '*', inspection: 'DeclaresVariable:zaz'}] }
 
     it { expect(result).to eq [
                                   {expectation: expectations[0], result: false},
@@ -43,12 +50,57 @@ describe CExpectationsHook do
     let(:expectations) { [
         {binding: '*', inspection: 'Declares:foo'},
         {binding: '*', inspection: 'Declares:bar'},
-        {binding: '*', inspection: 'Declares:baz'}] }
+        {binding: '*', inspection: 'Declares:zaz'}] }
 
     it { expect(result).to eq [
                                   {expectation: expectations[0], result: true},
                                   {expectation: expectations[1], result: true},
                                   {expectation: expectations[2], result: false}] }
+  end
+
+  describe 'Uses:' do
+    let(:code) { "int foo(int x, int y) { return bar(x + y); }" }
+    let(:expectations) { [{binding: '*', inspection: 'Uses:foo'},
+                          {binding: '*', inspection: 'Uses:bar'}] }
+
+    it { expect(result).to eq [{expectation: expectations[0], result: false},
+                               {expectation: expectations[1], result: true}] }
+  end
+
+  describe 'UsesIf' do
+    let(:code) { "int foo(int x, int y) { return x + y; }\nint bar(int x, int y) { if (x) return y; }" }
+    let(:expectations) { [{binding: 'foo', inspection: 'UsesIf'},
+                          {binding: 'bar', inspection: 'UsesIf'}] }
+
+    it { expect(result).to eq [{expectation: expectations[0], result: false},
+                               {expectation: expectations[1], result: true}] }
+  end
+
+  describe 'UsesWhile' do
+    let(:code) { "int foo(int x, int y) { return x + y; }\nint bar(int x, int y) { while (x) y; }" }
+    let(:expectations) { [{binding: 'foo', inspection: 'UsesWhile'},
+                          {binding: 'bar', inspection: 'UsesWhile'}] }
+
+    it { expect(result).to eq [{expectation: expectations[0], result: false},
+                               {expectation: expectations[1], result: true}] }
+  end
+
+  describe 'UsesFor' do
+    let(:code) { "int foo(int x, int y) { return x + y; }\nint bar(int x, int y) { for (x;y;x++); }" }
+    let(:expectations) { [{binding: 'foo', inspection: 'UsesForLoop'},
+                          {binding: 'bar', inspection: 'UsesForLoop'}] }
+
+    it { expect(result).to eq [{expectation: expectations[0], result: false},
+                               {expectation: expectations[1], result: true}] }
+  end
+
+  describe 'UsesSwitch' do
+    let(:code) { "int foo(int x, int y) { return x + y; }\nint bar(int x, int y) { switch (x) {}; }" }
+    let(:expectations) { [{binding: 'foo', inspection: 'UsesSwitch'},
+                          {binding: 'bar', inspection: 'UsesSwitch'}] }
+
+    it { expect(result).to eq [{expectation: expectations[0], result: false},
+                               {expectation: expectations[1], result: true}] }
   end
 
 end
